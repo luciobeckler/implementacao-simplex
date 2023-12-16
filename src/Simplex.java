12bfcedb6f9521018bd9eec5c075639c0d27a51b
Lucio_Beckler <luciobecklerp@gmail.com>
2023-12-15 22:13:18 -0300

    Lendo o arquivo fornecido

--- src/Simplex.java
+++ src/Simplex.java
@@ -1,5 +1,7 @@
 public class Simplex {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        File file = new File("src/instancia.txt");
+        file.ReadFile();
     }
 }
